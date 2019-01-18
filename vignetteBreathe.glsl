24916170140e3bed7d7ab3af5498d129281ff2e5
JoeThomas490 <joe_thomas490@hotmail.com>
2019-01-18 15:23:49 +0000

    Adding "map" function

--- vignetteBreathe.glsl
+++ vignetteBreathe.glsl
@@ -5,3 +5,9 @@ void main()
 
     gl_FragColor = vec4(uv.x, uv.y, 0.0, 1.0);
 }
+
+//Taken from : https://stackoverflow.com/questions/3451553/value-remapping
+float map(float value, float inputMin, float inputMax, float outputMin, float outputMax)
+{
+    return outputMin + (value - inputMin) * (outputMax - outputMin) / (inputMax - inputMin);
+}
